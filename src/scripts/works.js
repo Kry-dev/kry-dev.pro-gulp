9f38532062dcf651ec8a570a959d2ec16a2d7a42
kry-dev <yaroslavkryvda@gmail.com>
2017-11-18 14:43:50 +0200

    fix works page, create works.json/ create contact mail php

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -1,3 +1,4 @@
+console.log('works page');
 import {initPreloader} from './common/preloader.js';
 import {hamMenu} from './common/hamburgerMenu';
 import {toggleClass, smoothScrollTo} from './common/helpers';
@@ -24,13 +25,15 @@ upBtn.addEventListener('click',function () {
 
 const title = document.querySelector('.description-sl__title');
 const skills = document.querySelector('.description-sl__skills');
-const link = document.querySelector('.description-sl__link ');
 var mainSlide = [].slice.call(document.querySelectorAll('.photos-sl__item'));
+var worksLinks = [].slice.call(document.querySelectorAll('.description-sl__link'));
 
 /* Statement to all sliders. Main prop.  */
 var currentPos = 1;
 
 var worksNumber = mainSlide.length;
+var linksNumber = worksLinks.length;
+console.log(linksNumber);
 
 /* Button slider (previous work) */
 var prevBtnSlider = {
@@ -47,19 +50,19 @@ var nextBtnSlider = {
 
 /* Store for description slider */
 var projectDesc = [
-    { id:1, title: 'Site of brokerage and management services', skills: 'PDF TO HTML5, SCSS, GIT, WAFFLE.IO, RESPONSIVE LAYOUT, FOUNDATION, WEBPACK '},
-    { id:2, title: 'DEVOPS TALKS CONFERENCE', skills: 'PSD to HTML, CSS3, JS, JQUERY, PHOTOSHOP, RESPONSIVE, BOOTSTRAP, GULP, NPM, GIT'},
-    { id:3, title: 'Akreev designer portfolio', skills: 'PSD to HTML, CSS3, JS, JQUERY, PHOTOSHOP, RESPONSIVE, BOOTSTRAP, GULP, NPM, GIT'},
-    { id:4, title: 'Glenerin Inn & Spa', skills: 'FIXING, new layouts, slider, HTML, CSS3, JS, JQUERY, WORDPRESS'},
-    { id:5, title: 'GLOBAL MANUFACTURER OF STRETCH CEILING', skills: 'Fixing errors, MODX, psd to HTML, CSS3'},
-    { id:6, title: 'Real Estate Agent', skills: 'Wordpress, add news, some fixing, HTML, CSS, JQUERY, slider, video, PHOTOSHOP'},
-    { id:7, title: 'National ATM Wholesale', skills: 'OpenCart, customized layouts theme,psd to HTML, BOOTSTRAP, CSS, SCSS, JS, JQUERY, PHOTOSHOP, RESPONSIVE, GIT'},
-    { id:8, title: 'QUALITY REPLACEMENT WINDOWS', skills: 'Wordpress, new layouts, some fixing,psd to HTML, CSS, JQUERY, slider, PHOTOSHOP, GIT'},
-    { id:9, title: 'Personal Injury Lawyers', skills: 'Wordpress, new layouts, plugins, some fixing, HTML, CSS, JQUERY, PHOTOSHOP, GIT'},
-    { id:10, title: 'Custom Home Builders Hamilton', skills: 'Wordpress, new layouts, plugins, some fixing, HTML, CSS, JQUERY, PHOTOSHOP, GIT'},
-    { id:11, title: 'Business Analytics & Digital Marketing', skills: 'Webpack, Gulp, NPM, SCSS, PUG, FLEXBOX, BOOTSTRAP, JS, PSD to PUG, video, sliders'},
-    { id:12, title: 'ATM Super-STORE', skills: 'CUSTOMIZATION LAYOUT, HTML, CSS, JQUERY, OPENCART CMS, admin dashboard for back-end: GRUNT, SCSS, ES6, Bootstrap, Flexbox'},
-    { id:13, title: 'Southfield Windows and Doors', skills: 'Wordpress, new layouts, plugins, some fixing,psd to HTML, CSS, JQUERY, sliders, PHOTOSHOP, GIT'}
+    { id:1,  url:'https://goo.gl/YAHdZL', title: 'Site of brokerage and management services', skills: 'PDF TO HTML5, SCSS, GIT, WAFFLE.IO, RESPONSIVE LAYOUT, FOUNDATION, WEBPACK '},
+    { id:2,  url:'https://goo.gl/3EDo1c', title: 'DEVOPS TALKS CONFERENCE', skills: 'PSD to HTML, CSS3, JS, JQUERY, PHOTOSHOP, RESPONSIVE, BOOTSTRAP, GULP, NPM, GIT'},
+    { id:3,  url:'https://goo.gl/G1Od8d', title: 'Akreev designer portfolio', skills: 'PSD to HTML, CSS3, JS, JQUERY, PHOTOSHOP, RESPONSIVE, BOOTSTRAP, GULP, NPM, GIT'},
+    { id:4,  url:'https://goo.gl/BeYqy8', title: 'Glenerin Inn & Spa', skills: 'FIXING, new layouts, slider, HTML, CSS3, JS, JQUERY, WORDPRESS'},
+    { id:5,  url:'https://goo.gl/Xg29Kr', title: 'GLOBAL MANUFACTURER OF STRETCH CEILING', skills: 'Fixing errors, MODX, psd to HTML, CSS3'},
+    { id:6,  url:'https://goo.gl/dPHJJz', title: 'Real Estate Agent', skills: 'Wordpress, add news, some fixing, HTML, CSS, JQUERY, slider, video, PHOTOSHOP'},
+    { id:7,  url:'https://goo.gl/jN2mEz', title: 'National ATM Wholesale', skills: 'OpenCart, customized layouts theme,psd to HTML, BOOTSTRAP, CSS, SCSS, JS, JQUERY, PHOTOSHOP, RESPONSIVE, GIT'},
+    { id:8,  url:'https://goo.gl/sZTPY8', title: 'QUALITY REPLACEMENT WINDOWS', skills: 'Wordpress, new layouts, some fixing,psd to HTML, CSS, JQUERY, slider, PHOTOSHOP, GIT'},
+    { id:9,  url:'https://goo.gl/1enMTJ', title: 'Personal Injury Lawyers', skills: 'Wordpress, new layouts, plugins, some fixing, HTML, CSS, JQUERY, PHOTOSHOP, GIT'},
+    { id:10, url:'https://goo.gl/cxcyHo', title: 'Custom Home Builders Hamilton', skills: 'Wordpress, new layouts, plugins, some fixing, HTML, CSS, JQUERY, PHOTOSHOP, GIT'},
+    { id:11, url:'https://goo.gl/uJfKp4', title: 'Business Analytics & Digital Marketing', skills: 'Webpack, Gulp, NPM, SCSS, PUG, FLEXBOX, BOOTSTRAP, JS, PSD to PUG, video, sliders'},
+    { id:12, url:'https://goo.gl/wFVqaJ', title: 'ATM Super-STORE', skills: 'CUSTOMIZATION LAYOUT, HTML, CSS, JQUERY, OPENCART CMS, admin dashboard for back-end: GRUNT, SCSS, ES6, Bootstrap, Flexbox'},
+    { id:13, url:'https://goo.gl/GSQwux', title: 'Southfield Windows and Doors', skills: 'Wordpress, new layouts, plugins, some fixing,psd to HTML, CSS, JQUERY, sliders, PHOTOSHOP, GIT'}
 ];
 
 /* Need to correct displaing already loading document */
@@ -68,11 +71,13 @@ reDrow();
 /* On click emitting currentPos  */
 prevBtnSlider.elem.parentNode.addEventListener('click', function () {
     currentPos = changePos(prevBtnSlider.inc);
+    console.log(currentPos);
     reDrow();
 });
 nextBtnSlider.elem.parentNode.addEventListener('click', function () {
     currentPos = changePos(nextBtnSlider.inc);
     reDrow();
+    console.log(currentPos);
 });
 
 /**
@@ -100,7 +105,6 @@ function reDrow() {
 
     title.innerHTML = projectDesc[currentPos - 1].title;
     skills.innerHTML = projectDesc[currentPos - 1].skills;
-    // link.innerHTML = projectDesc[currentPos - 1].link;
     smoothTextAppearance(title);
     smoothTextAppearance(skills);
 }
